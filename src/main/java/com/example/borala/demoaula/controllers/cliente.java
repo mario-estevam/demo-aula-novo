80a128069271df84e25256639ff2a842871ed17b
Mario <mfreitas627@gmail.com>
2021-07-04 09:10:29 -0300

    upadte

--- src/main/java/com/example/borala/demoaula/controllers/cliente.java
+++ src/main/java/com/example/borala/demoaula/controllers/cliente.java
@@ -1,27 +1,72 @@
 package com.example.borala.demoaula.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 @Controller
 @RequestMapping ("/cliente")
 public class cliente {
 
+    conectaBanco conex = new conectaBanco();
+
+    @GetMapping
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, SQLException {
+
+        ArrayList<Alimento> array = new ArrayList<>();
+        array = AlimentoDao.listar();
+        response.getWriter().println("<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<table  action=\"/config\" border=\"1\">\n" +
+                "    <tr>\n" +
+                "        <td>ID</td>\n" +
+                "        <td>Nome do alimento</td>\n" +
+                "        <td>marca</td>\n" +
+                "        <td>pesagem</td>\n" +
+                "        <td>tipo</td>\n" +
+                "        <td>preco R$</td>\n" +
+                        "  <td>Carrinho</td>\n" +
+                "    </tr> \n"
+                );
+
+                for (int i=0; i<array.size(); i++) {
+
+                    response.getWriter().println("" +
+
+                            "<tr>\n" +
+                            "        <td> " + array.get(i).getID() +  "  </td>  \n" +
+                            "        <td>" + array.get(i).getNome() + "</td>   \n" +
+                            "        <td> " + array.get(i).getMarca() + "</td>    \n" +
+                            "        <td> " + array.get(i).getPesagem() + " </td>   \n" +
+                            "        <td>" + array.get(i).getTipo() + "</td>  \n" +
+                            "        <td>" + array.get(i).getPreco() + "  </td>   " +
+                            "        <td> <a href=\"/adicionarCarrinho?id="+array.get(i).getID()+"\">adicionar </td><br/> \n" +
+                            "   </tr> \n");
+                }
 
 
-    @PostMapping
-    public void doPost(HttpServletRequest request, HttpServletResponse response)throws IOException, ServletException {
-        String cliente = (String) request.getAttribute("cliente");
-        response.getWriter().println(cliente);
+                response.getWriter().println("\n" +
+                            "\n" +
+                            "</table>\n" +
+                            "<p><a href=\"/VerCarrinho\">Ver Carrinho</a></p>\n" +
+                            "</body>\n" +
+                            "</html>");
+
+
     }
 }
--- src/test/java/com/example/borala/demoaula/DemoAulaApplicationTests.java
+++ src/test/java/com/example/borala/demoaula/DemoAulaApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.borala.demoaula;
 
+import com.example.borala.demoaula.controllers.Alimento;
+import com.example.borala.demoaula.controllers.AlimentoDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,9 @@ class DemoAulaApplicationTests {
 
     @Test
     void contextLoads() {
+        AlimentoDao control = new AlimentoDao();
+        Alimento a = control.readforID("1");
+        System.out.println(a.getNome());
     }
 
 }
